7c318e807f70de71c2d2e19861c54ba959f3a915
Karl Theodor Ruby <63341740+Maelkiz@users.noreply.github.com>
2026-09-05 11:11:44 +0200

    Update fish config with new bindings and Starship setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,44 @@
-source (/usr/bin/starship init fish --print-full-init | psub)
+set -gx PATH "/home/karl/.pixi/bin" $PATH
+
+if not status is-interactive
+    return
+end
+
+# Starship
+source (/usr/local/bin/starship init fish --print-full-init | psub)
 
 function fish_greeting
     #fastfetch
 end
 
 function fish_user_key_bindings
-    bind ctrl-backspace backward-kill-word
-    bind ctrl-delete kill-word
-    bind ctrl-tab accept-autosuggestion
+    bind -M insert ctrl-backspace backward-kill-word
+    bind -M insert ctrl-delete kill-word
+    bind -M insert ctrl-tab accept-autosuggestion
 end
 
-alias ls='eza --icons'
-alias eduroam='nmcli --ask connection up eduroam'
-alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias ls='eza --group-directories-first --icons always'
+alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME' 
 
-set -gx PATH ~/.local/bin $PATH
+# Vim Mode
+fish_vi_key_bindings
+
+# Vim-style cursor
+set -g fish_cursor_insert line
+set -g fish_cursor_replace underscore
+set -g fish_cursor_visual block
+set -g fish_cursor_default block
+
+# Export Vim Mode environment variable for Starship prompt
+function update_fish_mode --on-variable fish_bind_mode
+    switch $fish_bind_mode
+        case default
+            set -gx FISH_MODE [NORMAL]
+        case insert
+            set -gx FISH_MODE [INSERT]
+        case visual
+            set -gx FISH_MODE [VISUAL]
+    end
+end
+
+update_fish_mode
